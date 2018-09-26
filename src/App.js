66470559af68bdf691c984209e7f24ac4bbb3bfb
critsmet <36425627+critsmet@users.noreply.github.com>
2018-09-26 11:48:54 -0400

    Error handling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SearchField from './SearchField'
+import UserDisplay from './UserDisplay'
 import './App.css';
 
 class App extends Component {
@@ -20,17 +21,27 @@ class App extends Component {
 
   timeout = null
 
+  handleErrors = (res) => {
+    if (!res.ok) {
+      throw Error(res.statusText);
+    }
+    return res;
+  }
+
   handleUpdateSearchField = search => {
 
     const updateUser = search => {
+      search === '' ? this.setState(prevState => ({...prevState, user: null})) :
       fetch('https://api.github.com/users/' + search)
+      .then(this.handleErrors)
       .then(res => res.json())
       .then(res => this.setState(prevState => {
         return {
           ...prevState,
-          user: res.data
+          user: res
         }
       }))
+      .catch(error => alert("Please enter a valid GitHub username"))
     }
 
     clearTimeout(this.timeout)
@@ -38,7 +49,7 @@ class App extends Component {
       this.timeout = setTimeout(function() {
         updateUser(search)
    }, 500);
-  }
+ }
 
   render() {
 
@@ -48,7 +59,7 @@ class App extends Component {
           <img id="github-logo" alt="github logo" src="https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png"/>
           <span>GitHub Mini Profile</span>
           <SearchField handleUpdateSearchField={this.handleUpdateSearchField}/>
-          { this.state.user !== null && <div>hi</div> }
+          { this.state.user !== null && <UserDisplay user={this.state.user} /> }
         </div>
       </div>
     );
---
+++ src/UserDisplay.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+export default class UserDisplay extends React.Component {
+
+  state = {repos: []}
+
+  fetchUserRepos = () => {
+    fetch(this.props.user.repos_url)
+    .then(res => res.json())
+    .then(res => this.setState({repos: res}))
+  }
+
+  componentDidMount(){
+    this.fetchUserRepos()
+  }
+
+  componentDidUpdate(){
+    this.fetchUserRepos()
+  }
+
+  render() {
+
+    const { user } = this.props
+
+    const formattedRepos = this.state.repos.map(repo => {
+      return <div key={repo.name}><a href={repo.html_url}>{repo.name}</a></div>
+    })
+
+    return (
+      <div id="user-display">
+        <div id="left-content">
+          <a href={user.html_url}>
+            <img alt={user.login + "photo"} src={user.avatar_url}/>
+          </a>
+          <div>
+            {user.name && <div>{user.name}<br/><br/></div>}
+            {user.bio && <div>{user.bio}<br/><br/></div>}
+            {user.location && <div>{user.location}<br/><br/></div>}
+            <a href={"http://" + user.blog}>{user.blog}</a>
+          </div>
+        </div>
+        <div id="right-content">
+          some repositories:
+          {formattedRepos}
+        </div>
+      </div>
+    )
+  }
+}
