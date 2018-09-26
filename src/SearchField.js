78c6d21f49c5c67326c8fef595703fe239186d17
critsmet <36425627+critsmet@users.noreply.github.com>
2018-09-26 12:26:05 -0400

    Format search field

--- src/SearchField.js
+++ src/SearchField.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const SearchField = props => {
   return (
-      <input id="search-field" onChange={(e) => props.handleUpdateSearchField(e.target.value)} type="text" placeholder="enter a github username here"/>
+      <input
+      id="search-field"
+      onChange={(e) => props.handleUpdateSearchField(e.target.value)}
+      type="text"
+      placeholder="enter a github username here"
+      />
   )
 }
 
